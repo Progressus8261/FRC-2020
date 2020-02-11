aaf56e30a75e99d1abc8941bde901ed110fb7b9d
Progressus8261 <60613766+Progressus8261@users.noreply.github.com>
2020-02-11 15:14:04 +0300

    Add files via upload

---
+++ java/frc/robot/Robot.java
@@ -0,0 +1,174 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import org.opencv.core.*;
+import org.opencv.imgproc.*;
+
+import edu.wpi.cscore.*;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.extensions.Servo;
+import frc.robot.extensions.*;
+
+public class Robot extends TimedRobot {
+
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "CAuto";
+  private String m_kAutoSelected;
+  
+  
+  
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  
+
+
+  private static Thread servoThread = new Thread(new ServoThread());
+  private static Thread servoThread2 = new Thread(new ServoSecThread());
+  private static Thread visionProcessingThread;
+
+
+  
+  private static VictorSP frontLeftMotor;
+  private static VictorSP frontRightMotor;
+  private static VictorSP rearLeftMotor;
+  private static VictorSP rearRightMotor;
+
+  public static Servo servo = new Servo(0);
+
+  public static Joystick joystick;
+
+  private static DifferentialDrive driver;
+
+  @Override
+  public void robotInit() 
+  {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+
+    joystick = new Joystick(0);
+
+    frontRightMotor = new VictorSP(0);
+    frontLeftMotor = new VictorSP(9);
+    rearLeftMotor = new VictorSP(2);
+    rearRightMotor = new VictorSP(3);
+
+    driver = new DifferentialDrive(frontLeftMotor, frontRightMotor);
+
+    visionProcessingThread = new Thread(() -> {
+      UsbCamera cam = CameraServer.getInstance().startAutomaticCapture(0);
+      cam.setFPS(30);
+      cam.setResolution(640, 480);
+
+      CvSink cvSink = CameraServer.getInstance().getVideo();
+      CvSource outputStream = CameraServer.getInstance().putVideo("Rectangle", 640, 480);
+
+      Mat mat = new Mat();
+
+      while(true) 
+      {
+        if(visionProcessingThread.isInterrupted()) break; // Check if this thread is ended
+
+        if (cvSink.grabFrame(mat) == 0) 
+        {
+					outputStream.notifyError(cvSink.getError()); // Notify if we got an error
+          continue;
+        }
+        
+        Imgproc.line(mat, 
+              new Point(315, 240), 
+              new Point(325, 240), 
+              new Scalar(255,0,0), 
+              5); // Simply draw the crosshairs horizontal line
+
+        Imgproc.line(mat, 
+              new Point(320, 235), 
+              new Point(320, 245), 
+              new Scalar(255,0,0), 
+              5); // Simply draw the crosshairs vertical line
+
+				outputStream.putFrame(mat); // Send the processed frame back to the stream
+      }
+    });
+
+    visionProcessingThread.start();
+  }
+  
+  @Override
+  public void robotPeriodic() 
+  {
+
+  }
+
+  @Override
+  public void autonomousInit() 
+  {
+    m_kAutoSelected = m_chooser.getSelected();
+  }
+
+  @Override
+  public void autonomousPeriodic() 
+  {
+
+    switch (m_kAutoSelected) 
+    {
+      case kCustomAuto:
+        break;
+
+      case kDefaultAuto:
+        break;
+
+      default:
+        break;
+    }
+
+  }
+
+  @Override
+  public void teleopInit() 
+  {
+    servoThread.start();  
+    servoThread2.start();
+  }
+
+  @Override
+  public void teleopPeriodic() 
+  {
+    
+    double speed = (joystick.getRawAxis(3) + 1) / 2;
+    double mVel = -joystick.getRawAxis(1) * speed;
+    double rVel = joystick.getRawAxis(4) * 0.60;
+
+    driver.arcadeDrive(mVel, rVel);
+
+    SmartDashboard.putNumber("Moving Speed: ", mVel);
+    SmartDashboard.putNumber("Rotation Speed: ", speed);
+    SmartDashboard.putNumber("FRONT RIGHT MOTOR: ", frontRightMotor.getSpeed());
+    SmartDashboard.putNumber("FRONT LEFT MOTOR: ", frontLeftMotor.getSpeed());
+  }
+
+  @Override
+  public void disabledInit() 
+  {
+    servoThread.interrupt();
+    servoThread2.interrupt(); 
+  }
+
+  @Override
+  public void testPeriodic() 
+  {
+
+  }
+}
